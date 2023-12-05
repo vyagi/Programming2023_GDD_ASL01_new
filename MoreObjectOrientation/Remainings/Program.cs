a88d099649b7a4a9efd8e5e3021ca42f0b144919
vyagi <marcin.jagiela@hotmail.com>
2023-12-05 19:05:26 +0100

    New project done

---
+++ MoreObjectOrientation/Remainings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Remainings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
