4f9cb1927953b5f673496bf4b233826db4df1c3a
vyagi <marcin.jagiela@hotmail.com>
2023-12-05 19:04:38 +0100

    Inheritance done

--- MoreObjectOrientation/Inheritance/Program.cs
+++ MoreObjectOrientation/Inheritance/Program.cs
@@ -2,11 +2,78 @@
 
 namespace Inheritance
 {
+    interface IPet
+    {
+        void Play();
+    }
+
+    abstract class Animal
+    {
+        public abstract void MakeSound();
+    }
+
+    class Dog : Animal, IPet
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Hau hau");
+        }
+        public void Play()
+        {
+            MakeSound();
+            MakeSound();
+            MakeSound();
+        }
+    }
+    class Cat : Animal, IPet
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Miau miau");
+        }
+
+        public void Play()
+        {
+            MakeSound();
+            MakeSound();
+        }
+    }
+
+    class Tiger : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Wrrrr");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var d = new Dog();
+            d.MakeSound();
+
+            var c = new Cat();
+            c.MakeSound();
+
+            MakeNoises(new Animal[] { d, c, d, new Cat(), new Dog(), new Tiger() });
+            PlayWithPets(new IPet[] { d, c, new Cat(), new Dog() });
+        }
+        public static void MakeNoises(Animal[] animals)
+        {
+            foreach (var animal in animals)
+            {
+                animal.MakeSound();
+            }
+        }
+
+        public static void PlayWithPets(IPet[] pets)
+        {
+            foreach(var pet in pets)
+            {
+                pet.Play();
+            }
         }
     }
 }
