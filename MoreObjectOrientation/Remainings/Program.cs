305d875bfc8307226c477c03dae910381ac91209
vyagi <marcin.jagiela@hotmail.com>
2023-12-05 19:18:56 +0100

    Structs vs classes done

--- MoreObjectOrientation/Remainings/Program.cs
+++ MoreObjectOrientation/Remainings/Program.cs
@@ -2,11 +2,28 @@
 
 namespace Remainings
 {
+    struct Student
+    {
+        public string Name;
+        public int Age;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Student s = new Student();
+            s.Name = "James";
+            s.Age = 40;
+
+            Modify(s);
+            Console.WriteLine($"{s.Name} is {s.Age}");
+        }
+
+        public static void Modify(Student student)
+        {
+            student.Name = "Mr. Bean";
+            student.Age = 50;
         }
     }
 }
